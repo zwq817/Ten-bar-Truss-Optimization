ca5e30f9a6349fa24f4dc4c8dfeee62402be6576
sylvia <zhangwq@solab.me.ntu.edu.tw>
2022-08-25 00:59:33 +0800

    算出各節點的兩個自由度的位移Q以及各桿件應力sigma

--- sol_TenBarTruss.m
+++ sol_TenBarTruss.m
@@ -1,3 +1,4 @@
+% 定義各參數數值
 coordinate = [18.28, 9.14; 18.28, 0; 9.14, 9.14;...
     9.14, 0; 0, 9.14; 0, 0];
 node = [3, 5; 1, 3; 4, 6; 2, 4; 3, 4;...
@@ -20,6 +21,33 @@ for z = 1:10
 end
 T = [node,E,A,L,c,s];
 
+% 開一個空白的剛性矩陣 (stiffness matrix)
 K = zeros(12,12);
 
-K = add_element(K, A, E, L, c, s, node)
+% 計算 stiffness matrix (可使用 add_element 函數)
+K = add_element(K, A, E, L, c, s, node);
+
+% 建立力矩陣
+F_matrix = [0; 0; 0; -10^7; 0; 0; 0; -10^7; 0; 0; 0; 0];
+
+% 建立空白位移矩陣
+Q = zeros(12,1);
+
+% 計算位移量 (F = KQ)
+% 因為node5.6是固定的,所以9~12 = 0
+% KQ = F
+F_matrix(9:12, :) = [];
+K(9:12, :) = [];
+K(:,9:12) = [];
+Q = K \ F_matrix;
+Q(9:12,:) = 0;
+
+% 計算應力 (stress) (可使用 compute_stress 函數)
+sigma = compute_stress(Q, E, L, c, s, node);
+
+
+
+
+
+
+
